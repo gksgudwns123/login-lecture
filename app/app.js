ce29b5fd8e74a103d97a3da3c65d8351b11e627c
HANHYEONGJUN <gksgudwns@kakao.com>
2022-10-16 09:21:52 +0900

    4th commit

--- app/app.js
+++ app/app.js
@@ -5,11 +5,12 @@ const express = require("express");
 const app = express();
 
 //라우팅
-const home = require("./src/routes/home/index");
+const home = require("./src/routes/home");
 
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home);
 
